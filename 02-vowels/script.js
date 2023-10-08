3974cd24f90c6af76862583f2dc81a4b9337338c
reypryma <refanopradana@gmail.com>
2023-10-08 11:07:04 +0700

    vowels

---
+++ 02-vowels/script.js
@@ -0,0 +1,23 @@
+function checkVowels() {
+    let text = document.getElementById('inputText').value;
+    let count = 0
+
+    text = text.toLowerCase()
+
+    for (let i = 0; i < text.length; i++) {
+        let  char = text.charAt(i)
+        if(isVowel(char)){
+            count++;
+        }
+    }
+
+    const result = document.getElementById('result')
+    result.textContent = 'Total Vowels: ' + count;
+
+
+}
+
+function isVowel(char) {
+    const vowels = ['a', 'i', 'e', 'o', 'u']
+    return vowels.includes(char)
+}
